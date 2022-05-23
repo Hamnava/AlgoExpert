0f9558b21a393b1b293b4fe00b20a6eabe2cbc3b
Nematullah Hamnava <nematullahhussaini626l@gmail.com>
2022-05-23 16:30:28 +0430

    finding subsequence of an array

---
+++ AlgoExpert/Basics/ValidateSubSequence.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgoExpert.Basics
+{
+    public class ValidateSubSequence
+    {
+        public void CheckSubSequence(int[] arrary, int[] subsequence)
+        {
+            int arraryIdx = 0;
+            int subsequenceIdx = 0;
+            List<int> list = new List<int>();
+
+            while (arraryIdx < arrary.Length && subsequenceIdx < subsequence.Length)
+            {
+                if (subsequence[subsequenceIdx] == arrary[arraryIdx])
+                {
+                    list.Add(subsequence[subsequenceIdx]);
+                    subsequenceIdx += 1;
+                }
+                else
+                {
+                    arraryIdx += 1;
+                }
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine("Your subsequence : "+ item);
+            }
+        }
+    }
+}
--- AlgoExpert/Program.cs
+++ AlgoExpert/Program.cs
@@ -9,11 +9,16 @@ using AlgoExpert.Basics;
 
 TowNumberSum sum = new TowNumberSum();
 
-int[] arr = new int[] { 1, 12, -3, 23, 15, 6, -7, 8, 11 };
+ValidateSubSequence seq = new ValidateSubSequence();
+
+int[] arr = new int[] { -1, 12, -3, 23, 15, 6, -7, 8, 11 };
+int[] arr2 = new int[] { -1, 12, -3, };
 int n = 20;
 //sum.TwoNumberSum(arr, n);
 
 //sum.TwoNumberSumHashSet(arr, n);
 
-sum.TowNumberSumTwoSide(arr, n);
+//sum.TowNumberSumTwoSide(arr, n);
+seq.CheckSubSequence(arr, arr2);
+
 
